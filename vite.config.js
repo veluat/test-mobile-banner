a1e4249912bc1142851f224fcede42450d578158
Julia <veluat@gmail.com>
2025-03-31 01:27:35 +0300

    build: fix vercel build (imports)

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,28 @@
-import {defineConfig} from 'vite'
-import {resolve} from 'path'
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: './src',
   publicDir: '../public',
+  base: './',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    assetsDir: './assets',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
       },
-    },
+      output: {
+        assetFileNames: '[name][extname]',
+        chunkFileNames: '[name].js',
+        entryFileNames: '[name].js'
+      }
+    }
   },
   server: {
     port: 3000,
     open: true,
-  },
+    host: true
+  }
 })
