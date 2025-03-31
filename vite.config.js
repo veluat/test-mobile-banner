d0ecd291bd675a3562b72de7ef08b331f9b52bb4
Julia <veluat@gmail.com>
2025-03-31 11:46:16 +0300

    build: fix build

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,20 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: './',
   root: './src',
+  base: '/',
   publicDir: '../public',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    assetsInlineLimit: 0,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
+      },
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        entryFileNames: 'js/[name].[hash].js',
       },
     },
   },
